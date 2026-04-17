7eb50f7cc0de2b814a27ccaba1de8d2374da90e4
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-17 10:55:39 -0300

    feat: adiciona hook de formulario de reset password

---
+++ src/features/auth/hooks/useResetPasswordForm.tsx
@@ -0,0 +1,47 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  resetPasswordSchema,
+  type ResetPasswordSchemaData,
+} from "../schemas/resetPassword.schema.ts";
+import { toast } from "sonner";
+import { useNavigate } from "react-router-dom";
+
+export function useResetPasswordForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ResetPasswordSchemaData>({
+    resolver: zodResolver(resetPasswordSchema),
+  });
+
+  const navigate = useNavigate();
+
+  const onSubmit = (data: ResetPasswordSchemaData) => {
+    console.log("RESET:", data);
+
+     toast.custom(() => (
+      <div className="bg-white rounded-2xl shadow-lg px-6 py-4 flex flex-col gap-1 w-96">
+        
+        <span className="text-green-600 font-semibold">
+          Senha redefinida!
+        </span>
+
+        <span className="text-gray-500 text-sm">
+          Agora você já pode fazer login
+        </span>
+
+      </div>
+    ));
+
+    navigate("/login");
+  };
+
+  return {
+    register,
+    handleSubmit,
+    errors,
+    onSubmit,
+  };
+}
