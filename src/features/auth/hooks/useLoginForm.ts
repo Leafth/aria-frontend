238de88ea6915581f444e9fe75c9658951cd1f2e
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-16 23:10:04 -0300

    feat: adiciona hook de formulário de login

---
+++ src/features/auth/hooks/useLoginForm.ts
@@ -0,0 +1,24 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema, type LoginSchemaData } from "../schemas/login.schema";
+
+export function useLoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginSchemaData>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = (data: LoginSchemaData) => {
+    console.log("LOGIN:", data);
+  };
+
+  return {
+    register,
+    handleSubmit,
+    errors,
+    onSubmit,
+  };
+}
