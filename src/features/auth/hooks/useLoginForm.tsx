54adc238e26e449017b514e3c3f56aa3fef254db
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-17 10:52:33 -0300

    refactor: remove toast do hook de login

--- src/features/auth/hooks/useLoginForm.tsx
+++ src/features/auth/hooks/useLoginForm.tsx
@@ -1,7 +1,6 @@
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { loginSchema, type LoginSchemaData } from "../schemas/login.schema";
-import { toast } from "sonner";
 
 export function useLoginForm() {
   const {
@@ -14,20 +13,6 @@ export function useLoginForm() {
 
   const onSubmit = (data: LoginSchemaData) => {
     console.log("LOGIN:", data);
-
-    toast.custom(() => (
-      <div className="bg-white rounded-2xl shadow-lg px-6 py-4 flex flex-col gap-1">
-        
-        <span className="text-green-600 font-semibold">
-          Sucesso!
-        </span>
-
-        <span className="text-gray-500 text-sm">
-          Enviamos o link de recuperação para o seu e-mail
-        </span>
-
-      </div>
-    ));
   };
 
   return {
