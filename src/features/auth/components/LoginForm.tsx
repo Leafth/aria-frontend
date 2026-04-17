f21516d449614d956e00609376421399bc37f570
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-17 00:10:51 -0300

    feat: adiciona o navigate para navegar ate a pagina de recuperacao de senha

--- src/features/auth/components/LoginForm.tsx
+++ src/features/auth/components/LoginForm.tsx
@@ -1,10 +1,13 @@
 import { Button } from "../../../shared/components/ui/button";
 import { Form } from "../../../shared/components/ui/form/Form";
 import { InputField } from "../../../shared/components/ui/input";
-import { useLoginForm } from "../hooks/useLoginForm";
+import { useLoginForm } from "../hooks/useLoginForm.tsx";
+import { useNavigate } from "react-router-dom";
 
 export function LoginForm() {
   const { register, handleSubmit, errors, onSubmit } = useLoginForm();
+
+  const navigate = useNavigate();
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
@@ -23,7 +26,10 @@ export function LoginForm() {
           error={errors.password?.message}
         />
 
-        <span className="text-sm text-right cursor-pointer">
+        <span
+          className="text-sm text-right cursor-pointer"
+          onClick={() => navigate("/forgot-password")}
+        >
           Esqueci minha senha
         </span>
       </div>
