9c026f12b7538f462b96d52c3d70b9748c59d612
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-16 22:10:14 -0300

    feat: define estilos do componente InputField

---
+++ src/shared/components/ui/input/InputField.styles.ts
@@ -0,0 +1,15 @@
+export const container = "flex flex-col gap-1";
+
+export const inputWrapper = "relative";
+
+export const inputBase =
+  "w-full pt-6 pb-1 px-4 rounded-lg border border-gray-400 bg-white text-black text-sm outline-none transition-all focus:border-primary-600 focus:ring-1 focus:ring-primary-200";
+
+export const inputError =
+  "border-red-500 focus:border-red-500 focus:ring-red-200";
+
+export const labelStyle =
+  "absolute left-4 top-2 text-xs pointer-events-none text-text-primary";
+
+export const iconButton =
+  "absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-gray-600";
