c25536a0d612d6ddb370910a5f25e4b0660821b1
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-17 22:05:54 -0300

    feat: adiciona a sidebar ao layout da pagina

--- src/shared/components/layout/MainLayout.tsx
+++ src/shared/components/layout/MainLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import { Sidebar } from "../ui/sidebar";
 
 export function MainLayout() {
   return (
     <div>
-      <main>
+      <main className="flex bg-background-primary gap-2">
+        <Sidebar />
         <Outlet />
       </main>
     </div>
