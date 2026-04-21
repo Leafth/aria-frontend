5ee039c4ff866b331e9146c73932e71b1bc4ab60
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-21 12:08:06 -0300

    feat: adiciona hook customizado para logout

---
+++ src/features/auth/hooks/useLogout.tsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+import { useQueryClient } from "@tanstack/react-query";
+import { logout } from "../services/auth.service";
+
+export function useLogout() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  async function handleLogout() {
+    try {
+      await logout();
+    } catch (error) {
+      console.error("Erro ao fazer logout:", error);
+    } finally {
+      queryClient.removeQueries({ queryKey: ["me"] });
+
+      navigate("/login", { replace: true });
+    }
+  }
+
+  return { handleLogout };
+}
