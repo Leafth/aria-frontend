5177d1e20d040f93b41a46f2e7f89455a7f9b20f
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-21 12:04:31 -0300

    feat: implementa service de autenticacao (login, me e logout)

--- src/features/auth/services/auth.service.ts
+++ src/features/auth/services/auth.service.ts
@@ -2,6 +2,17 @@ import { api } from "../../../lib/api";
 import type { AuthData } from "../types/auth.types";
 
 export async function login(data: AuthData) {
-  const res = await api.post("/login", data);
+  const res = await api.post("/auth/login", {
+    auth: data,
+  });
   return res.data;
+}
+
+export async function getMe() {
+  const response = await api.get("/me");
+  return response.data;
+}
+
+export async function logout() {
+  await api.delete("/auth/logout");
 }
