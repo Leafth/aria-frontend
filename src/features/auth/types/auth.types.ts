199318abb7af123e16ce41e645283e19e5c56ae3
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-22 22:47:07 -0300

    feat: adiciona os tipos de dados de reset password

--- src/features/auth/types/auth.types.ts
+++ src/features/auth/types/auth.types.ts
@@ -2,3 +2,9 @@ export interface AuthData {
   email: string;
   password: string;
 }
+
+export interface ResetPasswordData {
+  reset_password_token: string;
+  password: string;
+  password_confirmation: string;
+}
