2ceae57907cdce8a1d736e6ba333f5d685b907cf
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-23 14:35:21 -0300

    feat: implementa hook de criacao de companies

---
+++ src/features/reproductive-support/hooks/useCreateCompany.ts
@@ -0,0 +1,19 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { createCompany } from "../services/company.service";
+import { toast } from "sonner";
+
+export function useCreateCompany(onSuccessClose?: () => void) {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: createCompany,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["companies"] });
+      toast.success("Empresa cadastrada com sucesso");
+      onSuccessClose?.();
+    },
+    onError: () => {
+      toast.error("Erro ao cadastrar empresa");
+    },
+  });
+}
