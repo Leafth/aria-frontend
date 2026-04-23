67a0281eeb5defce5e86ae6bfb7a87b7fced9434
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-23 14:25:19 -0300

    feat: adiciona tipo ao modal

--- src/features/reproductive-support/components/ModalForm.tsx
+++ src/features/reproductive-support/components/ModalForm.tsx
@@ -1,24 +1,81 @@
 import { Button, InputField, Modal } from "../../../shared";
+import { useCreateCompany } from "../hooks/useCreateCompany";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { companySchema, type CompanyFormData } from "../schemas/company.schema";
 
 interface ModalFormProps {
   open: boolean;
   onClose: () => void;
+  type: "bull" | "company";
 }
 
-export function ModalForm({ open, onClose }: ModalFormProps) {
+export function ModalForm({ open, onClose, type }: ModalFormProps) {
+  const isCompany = type === "company";
+
+  const { mutateAsync, isPending } = useCreateCompany(onClose);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<CompanyFormData>({
+    resolver: zodResolver(companySchema),
+  });
+
+  const onSubmit = async (data: CompanyFormData) => {
+    if (!isCompany) return;
+
+    await mutateAsync(data);
+    reset();
+  };
+
+  const title = isCompany ? "Cadastrar Empresa" : "Cadastrar Touro";
+  const buttonLabel = isCompany ? "Cadastrar Empresa" : "Cadastrar Touro";
+
   return (
     <Modal
       open={open}
-      onClose={onClose}
-      title="Cadastrar Empresa"
-      footerContent={<Button className="w-full">Cadastrar</Button>}
+      onClose={() => {
+        reset();
+        onClose();
+      }}
+      title={title}
+      footerContent={
+        <Button
+          className="w-full"
+          onClick={handleSubmit(onSubmit)}
+          disabled={isPending}
+        >
+          {isPending ? "Salvando..." : buttonLabel}
+        </Button>
+      }
     >
-      <InputField label="Nome da Empresa" placeholder="ex: Strong Taurus" />
+      {isCompany ? (
+        <>
+          <InputField
+            label="Nome da Empresa*"
+            placeholder="ex: Strong Taurus"
+            {...register("name")}
+            error={errors.name?.message}
+          />
 
-      <InputField
-        label="Descrição"
-        placeholder="ex: Empresa da Santa Catarina"
-      />
+          <InputField
+            label="Descrição"
+            placeholder="ex: Empresa da Santa Catarina"
+            {...register("description")}
+            error={errors.description?.message}
+          />
+        </>
+      ) : (
+        <>
+          <InputField label="Número do Brinco*" />
+          <InputField label="Nome do Touro*" />
+          <InputField label="Raça*" />
+        </>
+      )}
     </Modal>
   );
 }
