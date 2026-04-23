7ad0a4146bd7298d8968ccf1e5782a9991609de1
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-23 14:22:19 -0300

    refactor: muda o de ingles para portugues texto da tabela

--- src/shared/components/ui/table/DataTablePagination.tsx
+++ src/shared/components/ui/table/DataTablePagination.tsx
@@ -9,7 +9,7 @@ export function DataTablePagination() {
   return (
     <div className="flex items-center gap-14">
       <div className="flex items-center gap-2">
-        <small>Rows per page</small>
+        <small>Linhas por página</small>
         <Select
           value={String(table.getState().pagination.pageSize)}
           onValueChange={value => {
@@ -36,7 +36,7 @@ export function DataTablePagination() {
       </div>
 
       <small>
-        Page {table.getState().pagination.pageIndex + 1} of {table.getPageCount()}
+        Página {table.getState().pagination.pageIndex + 1} de {table.getPageCount()}
       </small>
 
       <div className="space-x-2">
