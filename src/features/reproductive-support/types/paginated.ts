e5bbf298f29dc889d45f5c579da2ff13e0cb2340
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-23 14:51:24 -0300

    feat: adiciona tipagem para os parametro de paginacao

---
+++ src/features/reproductive-support/types/paginated.ts
@@ -0,0 +1,10 @@
+export interface PaginatedResponse<T> {
+  data: T[];
+  meta: {
+    current_page: number;
+    next_page: number | null;
+    prev_page: number | null;
+    total_pages: number;
+    total_count: number;
+  };
+}
