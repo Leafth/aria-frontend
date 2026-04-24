123da333b7a20f9e088666f8cc3cf48b79526934
TobiasTac <tobias.vida@live.com>
2026-04-23 22:45:34 -0300

    feat: implementa servico de criar bull

--- src/features/reproductive-support/services/bull.service.ts
+++ src/features/reproductive-support/services/bull.service.ts
@@ -6,3 +6,8 @@ export async function getBulls(params: BullParams) {
   const res = await api.get<PaginatedResponse<BullDTO>>("/bulls", { params });
   return res.data;
 }
+
+export async function createBull(data: Omit<BullDTO, "id" | "company">) {
+  const response = await api.post("/bulls", { bull: data });
+  return response.data;
+}
