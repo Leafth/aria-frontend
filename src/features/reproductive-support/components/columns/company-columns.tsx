62ff558f409f7d0aaab45522699d036cad43b022
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-23 23:53:20 -0300

    feat: adiciona ações de editar e excluir na tabela de company

--- src/features/reproductive-support/components/columns/company-columns.tsx
+++ src/features/reproductive-support/components/columns/company-columns.tsx
@@ -1,58 +1,64 @@
-import { Button } from '@/components/ui/button';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
-import { DataTableColumnHeader } from '@/shared/components/ui/table/DataTableColumnHeader';
-import type { ColumnDef } from '@tanstack/react-table';
-import { Edit2Icon, EllipsisIcon, Trash2Icon } from 'lucide-react';
-import { type CompanyDTO } from '../../types/reproductive-support.types';
+// components/columns/company-columns.ts
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { DataTableColumnHeader } from "@/shared/components/ui/table/DataTableColumnHeader";
+import type { ColumnDef } from "@tanstack/react-table";
+import { Edit2Icon, EllipsisIcon, Trash2Icon } from "lucide-react";
+import type { CompanyDTO } from "../../types/reproductive-support.types";
 
-export const companyColumns: ColumnDef<CompanyDTO>[] = [
-  {
-    accessorKey: 'name',
-    header: ({ column }) => <DataTableColumnHeader column={column} title="Nome" />,
-    meta: {
-      nameInFilters:'Nome',
+export function getCompanyColumns(
+  onDeleteClick: (company: CompanyDTO) => void,
+  onEditClick: (company: CompanyDTO) => void,
+): ColumnDef<CompanyDTO>[] {
+  return [
+    {
+      accessorKey: "name",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Nome" />
+      ),
     },
-    filterFn: 'equalsString',
-  },
-  {
-    accessorKey: 'description',
-    header: ({ column }) => <DataTableColumnHeader column={column} title="Descrição" />,
-    meta: {
-      nameInFilters:'Descrição',
+    {
+      accessorKey: "description",
+      header: ({ column }) => (
+        <DataTableColumnHeader column={column} title="Descrição" />
+      ),
     },
-  },
-  {
-    id: 'actions',
-    size: 80,
-    enableColumnFilter: false,
-    enableGlobalFilter: false,
-    enableHiding: false,
-    enableResizing: false,
-    enableMultiSort: false,
-    enableSorting: false,
-    cell: ({ row }) => {
-      const company = row.original;
+    {
+      id: "actions",
+      cell: ({ row }) => {
+        const company = row.original;
 
-      return (
-        <div className="flex justify-end">
+        return (
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <Button variant="ghost" size="sm">
+              <Button variant="ghost" size="sm" className="cursor-pointer">
                 <EllipsisIcon className="size-4" />
               </Button>
             </DropdownMenuTrigger>
 
             <DropdownMenuContent>
-              <DropdownMenuItem onSelect={() => alert(`Edit ${company.name}`)}>
+              <DropdownMenuItem
+                onSelect={() => onEditClick(company)}
+                className="cursor-pointer"
+              >
                 <Edit2Icon className="size-4" /> Edit
               </DropdownMenuItem>
-              <DropdownMenuItem onSelect={() => alert(`Delete ${company.name}`)}>
+
+              <DropdownMenuItem
+                onSelect={() => onDeleteClick(company)}
+                className="cursor-pointer"
+              >
                 <Trash2Icon className="size-4" /> Delete
               </DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
-        </div>
-      );
+        );
+      },
     },
-  },
-];
+  ];
+}
