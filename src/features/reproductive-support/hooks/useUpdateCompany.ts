1260f41b19415a3a14813b66b4eb06c81c9b1952
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-23 23:48:29 -0300

    feat: adiciona hook de atualizar company

---
+++ src/features/reproductive-support/hooks/useUpdateCompany.ts
@@ -0,0 +1,16 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateCompany } from "../services/company.service";
+
+export function useUpdateCompany(onSuccess?: () => void) {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ id, data }: { id: number; data: any }) =>
+      updateCompany(id, data),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["companies"] });
+      onSuccess?.();
+    },
+  });
+}
