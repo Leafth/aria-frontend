0fbf8ad6b37127268a28bf05386f323c28211cb8
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-25 17:33:29 -0300

    reafctor: organiza modais em uma pasta separada em components

---
+++ src/features/reproductive-support/components/modals/CompanyModalForm.tsx
@@ -0,0 +1,77 @@
+import { Button, InputField, Modal } from "../../../../shared";
+import { useSaveCompany } from "../../hooks/companies/useSaveCompany";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import {
+  companySchema,
+  type CompanyFormData,
+} from "../../schemas/company.schema";
+
+interface Props {
+  open: boolean;
+  onClose: () => void;
+  initialData?: (CompanyFormData & { id?: number }) | null;
+}
+
+export function CompanyModalForm({ open, onClose, initialData }: Props) {
+  const isEditing = !!initialData;
+
+  const { save, isPending } = useSaveCompany(onClose);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<CompanyFormData>({
+    resolver: zodResolver(companySchema),
+  });
+
+  useEffect(() => {
+    if (initialData) reset(initialData);
+    else reset();
+  }, [initialData, reset]);
+
+  const onSubmit = async (data: CompanyFormData) => {
+    await save(data, initialData?.id);
+    onClose();
+  };
+
+  return (
+    <Modal
+      open={open}
+      onClose={() => {
+        reset();
+        onClose();
+      }}
+      title={isEditing ? "Editar Empresa" : "Cadastrar Empresa"}
+      footerContent={
+        <Button
+          className="w-full"
+          onClick={handleSubmit(onSubmit)}
+          disabled={isPending}
+        >
+          {isPending ? "Salvando..." : "Salvar"}
+        </Button>
+      }
+    >
+      <InputField
+        label="Nome da Empresa*"
+        placeholder="ex: Taurus"
+        {...register("name")}
+        error={errors.name?.message}
+        disabled={isPending}
+      />
+
+      <InputField
+        label="Descrição"
+        placeholder="ex: Empresa de Santa Catarina"
+        {...register("description")}
+        error={errors.description?.message}
+        disabled={isPending}
+      />
+    </Modal>
+  );
+}
