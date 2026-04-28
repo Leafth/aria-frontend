fb98d9061f0452c49f74c5813b5199d8691f6305
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-28 11:16:20 -0300

    feat: adiciona tipos do card de rebanho

---
+++ src/features/flock/components/flock-card/flock-card.type.ts
@@ -0,0 +1,15 @@
+export interface FlockCardProps {
+  name: string;
+  code: string;
+  breed: string;
+  phase: string;
+  age: string;
+  weight: string;
+  alerts: number;
+  goal: string;
+  currentWeight: number;
+  goalWeight: number;
+  lastWeighing: string;
+  events: number;
+  colorCard: string;
+}
