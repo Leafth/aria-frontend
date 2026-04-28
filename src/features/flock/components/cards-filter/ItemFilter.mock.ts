923201881c1593e8c29ebd42817594ce1431d4eb
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-28 11:15:14 -0300

    feat: adiciona mock dos dados do rebanho

---
+++ src/features/flock/components/cards-filter/ItemFilter.mock.ts
@@ -0,0 +1,14 @@
+type ColorItem = {
+  name: string;
+  color: string;
+};
+
+export const filterFlocks: ColorItem[] = [
+  { name: "Todas", color: "#E5E7EB" },
+  { name: "Bezerra", color: "#B32A9A" },
+  { name: "Garrota", color: "#622BD0" },
+  { name: "Novilha", color: "#28C2B0" },
+  { name: "Premípara", color: "#0004FF" },
+  { name: "Multiparta", color: "#FF7700" },
+  { name: "Inativa", color: "#71717A" },
+];
