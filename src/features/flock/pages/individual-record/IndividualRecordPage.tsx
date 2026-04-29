0ab59f469872f420877a30be7a7a985752550967
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-04-29 15:50:50 -0300

    feat: estrutura pagina de ficha individual

---
+++ src/features/flock/pages/individual-record/IndividualRecordPage.tsx
@@ -0,0 +1,65 @@
+import { Button, Header } from "@/shared";
+import { SquarePen } from "lucide-react";
+import { AnimalStatusCard } from "../../components/individual-record/AnimalStatusCard";
+import { IndividualForm } from "../../components/forms/IndividualForm";
+import { RecentHistoryCard } from "../../components/individual-record/RecentHistoryCard";
+
+export default function IndividualRecordPage() {
+  return (
+    <main className="flex flex-col gap-6 p-4 w-full">
+      <div>
+        <p className="text-gray-500 text-sm">Rebanho {">"} Estrela</p>
+      </div>
+      <header className="flex justify-between items-center">
+        <Header
+          title="Estrela"
+          description="Brinco: #BRC-044 Nasc: 12/03/2026"
+        />
+        <Button variant="ghost">
+          <SquarePen size={20} />
+          Ações
+        </Button>
+      </header>
+      <section>
+        <AnimalStatusCard
+          status="Aguardando Cio"
+          nextDate="04/07/2027"
+          weight="542 kg"
+          lastWeigh="04/07/2027"
+          phase="Bezerra"
+        />
+        <div className="flex justify-between mt-5">
+          <IndividualForm />
+          <RecentHistoryCard
+            items={[
+              {
+                title: "Confirmação de prenhez",
+                description: "Diagnóstico positivo por ultrassom",
+                date: "01/04/2026",
+                color: "#22c55e",
+              },
+              {
+                title: "Inseminação Artificial",
+                description: "Sêmen: Bull Power X1",
+                date: "12/08/2025",
+                color: "#2563eb",
+              },
+              {
+                title: "Cio Detectado",
+                description: "Diagnóstico positivo por ultrassom",
+                date: "10/04/2025",
+                color: "#f59e0b",
+              },
+              {
+                title: "Pesagem Registrada",
+                description: "+120kg",
+                date: "10/04/2025",
+                color: "#84cc16",
+              },
+            ]}
+          />
+        </div>
+      </section>
+    </main>
+  );
+}
