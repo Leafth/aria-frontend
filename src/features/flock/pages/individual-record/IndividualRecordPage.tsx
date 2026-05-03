39c6587b471a40662c99708c7475d516acf001f1
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-05-03 15:32:45 -0300

    feat: adiciona drop down menu e opcao de voltar para rebanho

--- src/features/flock/pages/individual-record/IndividualRecordPage.tsx
+++ src/features/flock/pages/individual-record/IndividualRecordPage.tsx
@@ -1,24 +1,60 @@
 import { Button, Header } from "@/shared";
-import { SquarePen } from "lucide-react";
+import { SquarePen, Pen, Trash2 } from "lucide-react";
 import { AnimalStatusCard } from "../../components/individual-record/AnimalStatusCard";
-import { IndividualForm } from "../../components/forms/IndividualForm";
+import { IndividualForm } from "../../components/forms/individual/IndividualForm";
 import { RecentHistoryCard } from "../../components/individual-record/RecentHistoryCard";
+import { useNavigate } from "react-router-dom";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export default function IndividualRecordPage() {
+  const navigate = useNavigate();
+
   return (
     <main className="flex flex-col gap-6 p-4 w-full">
       <div>
-        <p className="text-gray-500 text-sm">Rebanho {">"} Estrela</p>
+        <p className="text-gray-500 text-sm">
+          <span className="cursor-pointer hover:text-black" onClick={() => navigate("/flock")}>
+            Rebanho
+          </span>{" "}
+          {">"} Estrela
+        </p>
       </div>
       <header className="flex justify-between items-center">
         <Header
           title="Estrela"
           description="Brinco: #BRC-044 Nasc: 12/03/2026"
         />
-        <Button variant="ghost">
-          <SquarePen size={20} />
-          Ações
-        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">
+              <SquarePen size={20} />
+              Ações
+            </Button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem
+              onSelect={() => alert("Editar")}
+              className="cursor-pointer"
+            >
+              <Pen size={16} />
+              Editar Dados
+            </DropdownMenuItem>
+
+            <DropdownMenuItem
+              onSelect={() => alert("Excluir")}
+              className="cursor-pointer"
+            >
+              <Trash2 size={16} />
+              Inativar
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </header>
       <section>
         <AnimalStatusCard
