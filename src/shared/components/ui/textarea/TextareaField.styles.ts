7ae1f8a38dbf3f1eb1efac8d9725887a382a137e
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-05-03 15:34:52 -0300

    feat: adiciona estilos do componente textarea

---
+++ src/shared/components/ui/textarea/TextareaField.styles.ts
@@ -0,0 +1,14 @@
+export const container = "flex flex-col gap-1";
+
+export const textareaWrapper = "relative";
+
+export const textareaBase =
+  "w-full min-h-[100px] pt-6 pb-6 px-4 rounded-lg border border-gray-400 bg-white text-black text-sm outline-none resize-none transition-all focus:border-primary-600 focus:ring-1 focus:ring-primary-200";
+
+export const textareaError =
+  "border-red-500 focus:border-red-500 focus:ring-2 focus:ring-red-200";
+
+export const labelStyle =
+  "absolute left-4 top-2 text-xs pointer-events-none";
+
+export const counter = "absolute bottom-2 right-3 text-xs text-gray-400";
