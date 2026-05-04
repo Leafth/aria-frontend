0b11501e0c43397db4d2ff8d0c23170f2ad0b57d
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-05-04 10:22:26 -0300

    styles: coloca botoes na lateral do form

--- src/features/flock/components/forms/individual/IndividualForm.tsx
+++ src/features/flock/components/forms/individual/IndividualForm.tsx
@@ -3,16 +3,12 @@ import { CioForm } from "../sections/CioForm";
 import { useState } from "react";
 import { Button } from "@/shared";
 import { ChildbirthForm } from "../sections/ChildbirthForm";
+import { CoverageForm } from "../sections/CoverageForm";
 
 export function IndividualForm() {
   const [state, setState] = useState("Cio");
   return (
-    <div className="flex flex-col">
-      <div className="flex gap-3">
-        <Button onClick={() => setState("Cio")}>Cio</Button>
-        <Button onClick={() => setState("Coverage")}>Cobertura</Button>
-        <Button onClick={() => setState("Childbirth")}>Parto</Button>
-      </div>
+    <div className="flex">
       <article className="bg-white w-120 h-68 rounded-2xl">
         <header className="flex gap-2 bg-[#10B981]/20 rounded-t-2xl py-2 px-3">
           <Sparkles size={20} />
@@ -20,9 +16,14 @@ export function IndividualForm() {
         </header>
 
         {state === "Cio" && <CioForm />}
-        {state === "Coverage" && <p></p>}
-        {state === "Childbirth" && <ChildbirthForm/>}
+        {state === "Coverage" && <CoverageForm />}
+        {state === "Childbirth" && <ChildbirthForm />}
       </article>
+      <div className="flex flex-col gap-3">
+        <Button onClick={() => setState("Cio")} variant="ghost">Cio</Button>
+        <Button onClick={() => setState("Coverage")} variant="ghost">Cobertura</Button>
+        <Button onClick={() => setState("Childbirth")} variant="ghost">Parto</Button>
+      </div>
     </div>
   );
 }
