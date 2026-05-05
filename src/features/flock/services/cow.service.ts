61c41bc0928df277ea80335cc5a3769fead87fa3
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-05-05 18:14:28 -0300

    feat: adiciona os endpoints da api

---
+++ src/features/flock/services/cow.service.ts
@@ -0,0 +1,43 @@
+import { api } from "@/lib/api";
+
+import type {
+  Cow,
+  CreateCowDTO,
+  UpdateCowDTO,
+  CowFilters,
+  PaginatedCowsResponse,
+} from "../types/cow.types";
+
+export async function getCows(params?: CowFilters) {
+  const response = await api.get<PaginatedCowsResponse>("/cows", {
+    params,
+  });
+
+  return response.data;
+}
+
+export async function getCowById(id: string) {
+  const response = await api.get<Cow>(`/cows/${id}`);
+
+  return response.data;
+}
+
+export async function createCow(data: CreateCowDTO) {
+  const response = await api.post<Cow>("/cows", {
+    cow: data,
+  });
+
+  return response.data;
+}
+
+export async function updateCow(id: number, data: UpdateCowDTO) {
+  const response = await api.patch<Cow>(`/cows/${id}`, {
+    cow: data,
+  });
+
+  return response.data;
+}
+
+export async function deleteCow(id: number) {
+  await api.delete(`/cows/${id}`);
+}
