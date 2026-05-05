26c59b908ded781b84b1f7b43a28600e8d2ffd29
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-05-05 18:15:59 -0300

    refactor: retira elementos nao utilizados

--- src/features/flock/components/flock-card/FlockCard.tsx
+++ src/features/flock/components/flock-card/FlockCard.tsx
@@ -1,25 +1,19 @@
-import {
-  Calendar,
-  Weight,
-  ArrowRight,
-  CircleAlert,
-} from "lucide-react";
+import { Calendar, Weight, ArrowRight } from "lucide-react";
+
 import { type FlockCardProps } from "./flock-card.type";
 import { useNavigate } from "react-router-dom";
 
 export function FlockCard({
+  id,
   name,
   code,
   breed,
   phase,
   age,
   weight,
-  goal,
-  goalWeight,
   colorCard,
 }: FlockCardProps) {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div
@@ -43,30 +37,23 @@ export function FlockCard({
 
       <div className="grid grid-cols-2 gap-2 bg-gray-100 rounded-lg justify-around divide-x divide-gray-300">
         <div className="p-2 flex flex-col items-center">
-          <Calendar size={16} className="font-bold"/>
+          <Calendar size={16} className="font-bold" />
           <span className="text-xs mt-1 font-bold">{age}</span>
           <span className="text-[10px] text-gray-500">Idade</span>
         </div>
 
         <div className="bg-gray-100 rounded-lg p-2 flex flex-col items-center">
-          <Weight size={16} className="font-bold"/>
+          <Weight size={16} className="font-bold" />
           <span className="text-xs mt-1 font-bold">{weight}</span>
           <span className="text-[10px] text-gray-500">Peso</span>
         </div>
       </div>
 
-      <div className="flex justify-between text-sm">
-        <span className="text-green-600">● Meta ({goal})</span>
-        <span className="font-medium">{goalWeight}kg</span>
-      </div>
-
-      <div className="bg-orange-100 text-orange-600 border-l-4 border-orange-600 text-sm p-4 rounded-lg flex items-center gap-2">
-        <CircleAlert size={20} />
-        <span>Próxima da meta. Pese regularmente.</span>
-      </div>
-
       <div className="flex justify-end items-center text-sm text-gray-600 mt-8">
-        <div className="flex items-center gap-1 cursor-pointer hover:text-black" onClick={() => navigate(`/flock/individual/${code}`)}>
+        <div
+          className="flex items-center gap-1 cursor-pointer hover:text-black"
+          onClick={() => navigate(`/flock/individual/${id}`)}
+        >
           Ver detalhes <ArrowRight size={14} />
         </div>
       </div>
