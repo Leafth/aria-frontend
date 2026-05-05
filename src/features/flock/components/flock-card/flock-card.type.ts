4cdb6ead2a369dbeb7ac7bae53775a16b3b49054
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-05-05 18:15:22 -0300

    refactor: retira propriedades de flock card e adiciona id como string

--- src/features/flock/components/flock-card/flock-card.type.ts
+++ src/features/flock/components/flock-card/flock-card.type.ts
@@ -1,11 +1,10 @@
 export interface FlockCardProps {
+  id: string;
   name: string;
   code: string;
   breed: string;
   phase: string;
   age: string;
   weight: string;
-  goal: string;
-  goalWeight: number;
   colorCard: string;
 }
