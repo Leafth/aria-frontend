3a1b8bfa1b38d32d29203f615b14660b30d74e85
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-05-07 19:21:39 -0300

    refactor: retira estado de mudanca de formulario

--- src/features/flock/components/forms/individual/IndividualForm.tsx
+++ src/features/flock/components/forms/individual/IndividualForm.tsx
@@ -1,12 +1,10 @@
 import { Sparkles } from "lucide-react";
 import { CioForm } from "../sections/CioForm";
-import { useState } from "react";
-import { Button } from "@/shared";
-import { ChildbirthForm } from "../sections/ChildbirthForm";
-import { CoverageForm } from "../sections/CoverageForm";
+// import { ChildbirthForm } from "../sections/ChildbirthForm";
+// import { CoverageForm } from "../sections/CoverageForm";
 
 export function IndividualForm() {
-  const [state, setState] = useState("Cio");
+  const state = "Cio";
   return (
     <div className="flex">
       <article className="bg-white w-120 h-68 rounded-2xl">
@@ -16,13 +14,10 @@ export function IndividualForm() {
         </header>
 
         {state === "Cio" && <CioForm />}
-        {state === "Coverage" && <CoverageForm />}
-        {state === "Childbirth" && <ChildbirthForm />}
+        {/* {state === "Coverage" && <CoverageForm />}
+        {state === "Childbirth" && <ChildbirthForm />} */}
       </article>
       <div className="flex flex-col gap-3">
-        <Button onClick={() => setState("Cio")} variant="ghost">Cio</Button>
-        <Button onClick={() => setState("Coverage")} variant="ghost">Cobertura</Button>
-        <Button onClick={() => setState("Childbirth")} variant="ghost">Parto</Button>
       </div>
     </div>
   );
