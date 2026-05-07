3dccd375c74b2da6bdfc2345f886fdb20462e379
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-05-07 14:51:02 -0300

    feat: cria funcao que retorna fase do animal

---
+++ src/features/flock/utils/getPhaseLabel.ts
@@ -0,0 +1,13 @@
+import type { CowPhase } from "../types/cow.types";
+
+export function getPhaseLabel(phase: CowPhase) {
+  const labels: Record<CowPhase, string> = {
+    calf: "Bezerra",
+    heifer: "Garrota",
+    young: "Novilha",
+    primiparous: "Primípara",
+    multiparous: "Multiparta",
+  };
+
+  return labels[phase];
+}
