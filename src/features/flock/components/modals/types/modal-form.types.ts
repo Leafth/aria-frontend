3d9df4e026ef46816568b2b1e7f4e1a2239618c6
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-05-07 11:50:50 -0300

    feat: adiciona tipos ao arquivo separado

---
+++ src/features/flock/components/modals/types/modal-form.types.ts
@@ -0,0 +1,16 @@
+export interface ModalFormProps {
+  open: boolean;
+  onClose: () => void;
+}
+
+export type ApiErrorResponse = {
+  errors?: {
+    ear_tag?: string[];
+    name?: string[];
+    birth_date?: string[];
+    breed?: string[];
+    weight?: string[];
+    phase?: string[];
+    active?: string[];
+  };
+};
