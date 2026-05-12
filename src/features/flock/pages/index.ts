747b0a102b40ebb734e3c8e587ab4bd3f2910c71
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-05-12 14:23:04 -0300

    feat: adiciona export da pagina de historico completo com barrel exports

--- src/features/flock/pages/index.ts
+++ src/features/flock/pages/index.ts
@@ -1,2 +1,3 @@
 export {default as FlockPage } from "./flock-page/FlockPage"
-export {default as IndividualRecordPage } from "./individual-record/IndividualRecordPage"
+export {default as IndividualRecordPage } from "./individual-record-page/IndividualRecordPage"
+export {default as FullHistoryPage} from "./full-history-page/FullHistoryPage"
