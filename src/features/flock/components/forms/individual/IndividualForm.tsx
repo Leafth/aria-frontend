352dc0b54e55443f86261561bcf53c9262d66a42
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-05-13 14:04:00 -0300

    feat: adiciona controle de estado dos formularios

--- src/features/flock/components/forms/individual/IndividualForm.tsx
+++ src/features/flock/components/forms/individual/IndividualForm.tsx
@@ -1,10 +1,13 @@
 import { Sparkles } from "lucide-react";
 import { CioForm } from "../sections/CioForm";
-// import { ChildbirthForm } from "../sections/ChildbirthForm";
-// import { CoverageForm } from "../sections/CoverageForm";
+import { ChildbirthForm } from "../sections/ChildbirthForm";
+import { CoverageForm } from "../sections/CoverageForm";
+import { useState } from "react";
+import { PregnancyForm } from "../sections/PregnancyForm";
+import { Button } from "@/components/ui/button";
 
 export function IndividualForm() {
-  const state = "Cio";
+  const [state, setState] = useState("cio");
 
   return (
     <article className="bg-white w-full rounded-2xl overflow-hidden">
@@ -14,9 +17,36 @@ export function IndividualForm() {
       </header>
 
       <div className="p-4 sm:p-6">
-        {state === "Cio" && <CioForm />}
-        {/* {state === "Coverage" && <CoverageForm />}
-        {state === "Childbirth" && <ChildbirthForm />} */}
+        {state === "cio" && <CioForm />}
+        {state === "coverage" && <CoverageForm />}
+        {state === "childbirth" && <ChildbirthForm />}
+        {state === "pregnancy" && <PregnancyForm />}
+      </div>
+      <div>
+        <Button
+          onClick={() => setState("cio")}
+          className="cursor-pointer opacity-30"
+        >
+          Cio
+        </Button>
+        <Button
+          onClick={() => setState("coverage")}
+          className="cursor-pointer opacity-30"
+        >
+          Cobertura
+        </Button>
+        <Button
+          onClick={() => setState("childbirth")}
+          className="cursor-pointer opacity-30"
+        >
+          Parto
+        </Button>
+        <Button
+          onClick={() => setState("pregnancy")}
+          className="cursor-pointer opacity-30"
+        >
+          Prenhez
+        </Button>
       </div>
     </article>
   );
