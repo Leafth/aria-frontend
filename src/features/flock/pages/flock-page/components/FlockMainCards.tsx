136bf5cf1879a1b427095eca2446ed6331b99374
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-05-14 23:17:10 -0300

    feat: adiciona componente do conteudo principal da pagina

---
+++ src/features/flock/pages/flock-page/components/FlockMainCards.tsx
@@ -0,0 +1,28 @@
+import { FlockCard } from "../../../components/flock-card/FlockCard";
+import type { cowToFlockCard } from "../../../utils/cowToFlockCard";
+
+type FlockCardData = ReturnType<typeof cowToFlockCard>;
+
+interface FlockMainCardsProps {
+  cards: FlockCardData[];
+}
+
+export function FlockMainCards({ cards }: FlockMainCardsProps) {
+  return (
+    <div className="grid gap-5 grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4">
+      {cards.map((flock) => (
+        <FlockCard
+          key={flock.id}
+          id={flock.id}
+          name={flock.name}
+          code={flock.code}
+          breed={flock.breed}
+          phase={flock.phase}
+          age={flock.age}
+          weight={flock.weight}
+          colorCard={flock.colorCard}
+        />
+      ))}
+    </div>
+  );
+}
