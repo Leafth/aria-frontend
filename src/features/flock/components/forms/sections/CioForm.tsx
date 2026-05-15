711c81c5dadff2f1abe4617795aeaafbcb2eb88a
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-05-14 23:57:17 -0300

    feat: adiciona toggle de confirmaçao de cobertura

--- src/features/flock/components/forms/sections/CioForm.tsx
+++ src/features/flock/components/forms/sections/CioForm.tsx
@@ -1,6 +1,9 @@
 import { Button, InputField } from "@/shared";
+import { ToggleField } from "@/shared/components/ui/toggle/ToggleField";
+import { useState } from "react";
 
 export function CioForm() {
+  const [origin, setOrigin] = useState("local");
   return (
     <form className="flex flex-col gap-5">
       <div>
@@ -9,7 +12,16 @@ export function CioForm() {
           Registrar dados para confirmar cio
         </p>
       </div>
-      <InputField label="Data do Cio" type="date"/>
+      <ToggleField
+        label=""
+        value={origin}
+        onChange={setOrigin}
+        options={[
+          { label: "Sem Cobertura", value: "local" },
+          { label: "Com Cobertura", value: "company" },
+        ]}
+      />
+      <InputField label="Data do Cio" type="date" />
       <Button className="w-full">Confirmar Cio</Button>
     </form>
   );
