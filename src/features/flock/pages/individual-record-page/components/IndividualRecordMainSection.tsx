c35b9ad1bd88138ab9ea898bee3b36371a81a8a3
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-05-18 14:57:03 -0300

    feat: adiciona os novos campos de dados da busca de vaca

--- src/features/flock/pages/individual-record-page/components/IndividualRecordMainSection.tsx
+++ src/features/flock/pages/individual-record-page/components/IndividualRecordMainSection.tsx
@@ -2,13 +2,13 @@ import { useNavigate } from "react-router-dom";
 import { IndividualForm } from "../../../components/forms/individual/IndividualForm";
 import { AnimalStatusCard } from "../../../components/individual-record/AnimalStatusCard";
 import { RecentHistoryCard } from "../../../components/individual-record/recent-history/RecentHistoryCard";
-import type { Cow } from "../../../types/cow.types";
+import type { CowDetails } from "../../../types/cow.types";
 import { getPhaseLabel } from "../../../utils/getPhaseLabel";
 import { recentHistoryMock } from "../../../components/individual-record/recent-history/recent-history.mock";
 import { useChangeCowPhase } from "../../../hooks/useChangeCowPhase";
 
 interface IndividualRecordMainSectionProps {
-  cow: Cow;
+  cow: CowDetails;
   onRegisterWeight: () => void;
 }
 
@@ -24,12 +24,14 @@ export function IndividualRecordMainSection({
   return (
     <section className="flex flex-col gap-5">
       <AnimalStatusCard
-        nextDate="-"
-        weight={`${cow.weight}kg`}
-        lastWeigh="-"
-        phase={getPhaseLabel(cow.phase)}
-        currentPhase={cow.phase}
+        weight={`${cow.insights.weight_insight.current_weight ?? "-"}kg`}
+        lastWeigh={cow.insights.weight_insight.last_weighing_at ?? "-"}
+        phase={getPhaseLabel(cow.insights.phase_insight.current_phase)}
+        currentPhase={cow.insights.phase_insight.current_phase}
         onRegisterWeight={onRegisterWeight}
+        statusMessage={cow.insights.reproductive_status.message}
+        observation={cow.insights.reproductive_status.observation}
+        phaseSuggestion={cow.insights.phase_insight.message}
         isChangingPhase={isChangingPhase}
         onChangePhase={async (phase) => {
           await changeCowPhase({
@@ -41,10 +43,10 @@ export function IndividualRecordMainSection({
         }}
         isActive={cow.active}
       />
-      <div className="flex flex-col gap-5 xl:flex-row">
+      <div className="flex flex-col gap-5 sm:flex-row">
         {cow.active && (
           <div className="w-full xl:flex-1">
-            <IndividualForm cowId={cow.id}/>
+            <IndividualForm cowId={cow.id} />
           </div>
         )}
 
