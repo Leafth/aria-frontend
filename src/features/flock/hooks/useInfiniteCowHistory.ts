918617ef0939b53645d5c27fa7f554eab2d1baf7
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-05-20 23:13:44 -0300

    feat: adiciona hook com paginacao de scroll infinito

---
+++ src/features/flock/hooks/useInfiniteCowHistory.ts
@@ -0,0 +1,26 @@
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { getCowHistory } from "../services/cow.service";
+import type { CowHistoryFilters } from "../types/cow.types";
+
+export function useInfiniteCowHistory(
+  cowId?: string,
+  params?: Omit<CowHistoryFilters, "page">,
+) {
+  return useInfiniteQuery({
+    queryKey: ["cow-history", cowId, params],
+
+    queryFn: ({ pageParam }) =>
+      getCowHistory(cowId!, {
+        ...params,
+        page: pageParam,
+      }),
+
+    initialPageParam: 1,
+
+    getNextPageParam: (lastPage) => {
+      return lastPage.meta.next_page;
+    },
+
+    enabled: Boolean(cowId),
+  });
+}
