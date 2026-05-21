e78d91a0e82ac1639d0f6aff18cf599593eb8cc4
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-05-21 16:21:30 -0300

    feat: adiciona tipagem de id e toast de aviso

--- src/features/flock/hooks/useDeleteCow.ts
+++ src/features/flock/hooks/useDeleteCow.ts
@@ -1,16 +1,27 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-
 import { deleteCow } from "../services/cow.service";
+import { toast } from "sonner";
+
+interface DeleteCowParams {
+  id: string;
+}
 
 export function useDeleteCow() {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: (id: number) => deleteCow(id),
+    mutationFn: ({ id }: DeleteCowParams) => deleteCow(id),
+
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: ["cows"],
       });
+
+      toast.success("Matriz excluído com sucesso.");
+    },
+
+    onError: () => {
+      toast.error("Não foi possível excluir o animal.");
     },
   });
 }
