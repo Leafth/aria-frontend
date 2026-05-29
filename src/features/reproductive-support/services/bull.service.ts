f3af725a60bb9e032a64fc40f7a6b2778dcf55b0
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-05-28 23:24:17 -0300

    feat: adiciona tipagem de criacao e atualizacao de touros

--- src/features/reproductive-support/services/bull.service.ts
+++ src/features/reproductive-support/services/bull.service.ts
@@ -1,22 +1,27 @@
 import { api } from "@/lib/api";
 import type { PaginatedResponse } from "../types/paginated";
-import type { BullDTO, BullParams } from "../types/reproductive-support.types";
+import type {
+  BullDTO,
+  BullParams,
+  CreateBullDTO,
+  UpdateBullDTO,
+} from "../types/reproductive-support.types";
 
 export async function getBulls(params: BullParams) {
   const res = await api.get<PaginatedResponse<BullDTO>>("/bulls", { params });
   return res.data;
 }
 
-export async function createBull(data: Omit<BullDTO, "id" | "company">) {
+export async function createBull(data: CreateBullDTO) {
   const response = await api.post("/bulls", { bull: data });
   return response.data;
 }
 
-export async function updateBull(id: number, data: Omit<BullDTO, "id" | "company">) {
+export async function updateBull(id: string, data: UpdateBullDTO) {
   const response = await api.put(`/bulls/${id}`, { bull: data });
   return response.data;
 }
 
-export async function deleteBull(id: number) {
+export async function deleteBull(id: string) {
   await api.delete(`/bulls/${id}`);
 }
