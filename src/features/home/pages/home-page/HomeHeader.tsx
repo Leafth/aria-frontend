5a4a315efe14e0aee373adaaf03024f6100fdd29
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-06-15 22:12:25 -0300

    feat: adiciona componente de cabeçalho a pagina inicial

---
+++ src/features/home/pages/home-page/HomeHeader.tsx
@@ -0,0 +1,12 @@
+import { Header } from "@/shared";
+
+export function HomeHeader() {
+  return (
+    <header>
+      <Header
+        title="Página Principal"
+      />
+      
+    </header>
+  );
+}
