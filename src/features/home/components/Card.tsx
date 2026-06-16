dfd841e4ba625e6e6924497a8a135eeb6a675b79
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-06-15 22:13:31 -0300

    feat: adiciona componente de card

---
+++ src/features/home/components/Card.tsx
@@ -0,0 +1,16 @@
+import type { ReactNode } from "react";
+
+interface CardProps {
+  title: string;
+  children: ReactNode;
+}
+export function Card({ title, children }: CardProps) {
+  return (
+    <div className="flex flex-col bg-white px-8 py-5 w-full rounded-lg">
+      <header className="mb-5">
+        <h1 className="font-semibold text-lg">{title}</h1>
+      </header>
+      {children}
+    </div>
+  );
+}
