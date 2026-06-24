d0f1e9786005583a831c36719c89ffe4bf70a105
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-06-24 00:54:38 -0300

    feat: adiciona os principais componentes de pagina de relatorios

--- src/features/reports/pages/report-page/components/ReportContent.tsx
+++ src/features/reports/pages/report-page/components/ReportContent.tsx
@@ -1,13 +1,55 @@
+import {
+  ReportsIndicatorsTable,
+  ChartPieInteractive,
+  ChartBarMixed,
+  ChartLineMultiple,
+} from "@/features/reports/components";
 
-import { ReportsIndicatorsTable } from "@/features/reports/components";
 import { ReportHeader } from "./ReportHeader";
 import { reportsTableMock } from "@/features/reports/mock/ReportsTableMock";
 
 export function ReportContent() {
   return (
-    <main className="flex flex-col gap-6 p-4 w-full">
+    <main className="flex w-full flex-col gap-6 p-4">
       <ReportHeader />
+
       <ReportsIndicatorsTable data={reportsTableMock} />
+
+      <section className="grid w-full grid-cols-1 gap-10 lg:grid-cols-2">
+        <ChartPieInteractive
+          title="Taxa de Diagnóstico"
+          description="Últimos 30 dias"
+          defaultValue="diagnosis"
+          options={[
+            {
+              value: "diagnosis",
+              label: "Diagnóstico",
+              confirmedLabel: "Positivos",
+              deniedLabel: "Negativos",
+              confirmedValue: 20,
+              deniedValue: 5,
+            },
+          ]}
+        />
+        <ChartPieInteractive
+          title="Taxa de Diagnóstico"
+          description="Últimos 30 dias"
+          defaultValue="diagnosis"
+          options={[
+            {
+              value: "diagnosis",
+              label: "Diagnóstico",
+              confirmedLabel: "Positivos",
+              deniedLabel: "Negativos",
+              confirmedValue: 20,
+              deniedValue: 5,
+            },
+          ]}
+        />
+      </section>
+
+      <ChartBarMixed />
+      <ChartLineMultiple />
     </main>
   );
 }
