bbe614df32e2b1ea831a31ad5c3c75fc70b7756f
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-06-24 00:54:11 -0300

    feat: adiciona dados de testes para o grafico

---
+++ src/features/reports/mock/ReportsDonutChartMock.ts
@@ -0,0 +1,22 @@
+export const chartData = [
+  {
+    name: "prenhas",
+    value: 2,
+    fill: "#003AFF",
+  },
+  {
+    name: "vazias",
+    value: 4,
+    fill: "#FF7700",
+  },
+  {
+    name: "cio",
+    value: 1,
+    fill: "#B300A9",
+  },
+  {
+    name: "inseminadas",
+    value: 3,
+    fill: "#00A5C2",
+  },
+];
