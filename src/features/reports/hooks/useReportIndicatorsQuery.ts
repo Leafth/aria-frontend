d7238529612c259ba683dae4e7da1c8349f5bde2
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-06-25 15:29:01 -0300

    feat: adiciona timer e parametro para a busca dos dados

--- src/features/reports/hooks/useReportIndicatorsQuery.ts
+++ src/features/reports/hooks/useReportIndicatorsQuery.ts
@@ -1,12 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
-
-import { mapReportIndicators } from "../mappers/report-indicators.mapper";
+import type { ReportPeriod } from "../components/period-filter/types";
 import { reportsService } from "../services/reports.service";
+import { mapReportIndicators } from "../mappers/report-indicators.mapper";
 
-export function useReportIndicatorsQuery() {
+const SEARCH_DELAY_IN_MS = 800;
+
+function wait(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export function useReportIndicatorsQuery(period: ReportPeriod) {
   return useQuery({
-    queryKey: ["reports", "reproductive-indicators"],
-    queryFn: reportsService.getReproductiveIndicators,
+    queryKey: ["reports", "reproductive-indicators", period],
+    queryFn: async () => {
+      await wait(SEARCH_DELAY_IN_MS);
+
+      return reportsService.getReproductiveIndicators({ period });
+    },
     select: mapReportIndicators,
+    retry: false,
   });
 }
