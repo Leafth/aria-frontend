f2415e1a0b9487f0ca9c7645e7bab815b53e61c6
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2026-06-25 17:45:38 -0300

    feat: adiciona endpoint de contador de eventos

--- src/features/reports/services/reports.service.ts
+++ src/features/reports/services/reports.service.ts
@@ -1,9 +1,12 @@
 import { api } from "@/lib/api";
+
+import { mapReportPeriodToParams } from "../mappers/report-period.mapper";
+
 import type {
   GetReproductiveIndicatorsParams,
+  ReportFunnelApi,
   ReportIndicatorsResponseApi,
 } from "../types/reports-api.types";
-import { mapReportPeriodToParams } from "../mappers/report-period.mapper";
 
 export const reportsService = {
   getReproductiveIndicators: async ({
@@ -11,6 +14,19 @@ export const reportsService = {
   }: GetReproductiveIndicatorsParams) => {
     const response = await api.get<ReportIndicatorsResponseApi>(
       "/dashboard/reproductive-indicators",
+      {
+        params: mapReportPeriodToParams(period),
+      },
+    );
+
+    return response.data;
+  },
+
+  getReproductiveFunnel: async ({
+    period,
+  }: GetReproductiveIndicatorsParams) => {
+    const response = await api.get<ReportFunnelApi>(
+      "/dashboard/event-counts",
       {
         params: mapReportPeriodToParams(period),
       },
